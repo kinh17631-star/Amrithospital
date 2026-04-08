981fcdf243f86846be6c747ef5fb6d0efbd58961
kinh17631-star <kinh17631@gmail.com>
2026-04-08 12:02:46 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    const elements = document.querySelectorAll(".slide-left, .slide-right");
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    });
+
+    elements.forEach((el) => observer.observe(el));
+  }, []);
+
   return <Component {...pageProps} />;
 }
